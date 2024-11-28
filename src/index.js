d3f4bb93ff2db998b40e0c115249733f0690f0e7
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-11-28 13:31:31 +0530

    feat: kubernetes init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import os from 'node:os';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello Docker !',
+    hostname: os.hostname(),
+    arch: os.arch(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server listening at: http://${hostname}:${port}`);
+});
