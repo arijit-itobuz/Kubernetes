a90c202cb2d3d2e14b1847c1ff9ae1ddf5d4072c
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-12-09 15:34:35 +0530

    feat :added mongodb

--- src/index.js
+++ src/index.js
@@ -1,39 +1,64 @@
 import express from 'express';
 import os from 'node:os';
+import { MongoClient } from 'mongodb';
 
 const app = express();
-
 const hostname = 'localhost';
 const port = process.env.PORT;
 const app_env = process.env.APP_ENV;
 const version = process.env.VERSION;
-const db_url = process.env.MONGODB_URL;
+const mongodb_url = process.env.MONGODB_URL;
+const mongodb_client = new MongoClient(mongodb_url);
 
-app.get('/', async (req, res) => {
-  try {
-    return res.status(200).json({
-      success: true,
-      message: 'Hello Kubernetes !',
-      app_env: app_env,
-      hostname: os.hostname(),
-      arch: os.arch(),
-      version: version,
-    });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error?.message });
-  }
-});
+await mongodb_client.connect();
 
-app.get('/nginx', async (req, res) => {
-  try {
-    const url = 'http://nginx:80';
-    const response = await fetch(url);
-    return res.status(200).send(await response.text());
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error?.message });
-  }
-});
+async function main() {
+  app.get('/', async (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        message: 'Hello Kubernetes !',
+        app_env: app_env,
+        hostname: os.hostname(),
+        arch: os.arch(),
+        version: version,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-app.listen(port, () => {
-  console.log(`server listening at: http://${hostname}:${port}`);
-});
+  app.get('/nginx', async (req, res) => {
+    try {
+      const url = 'http://nginx:80';
+      const response = await fetch(url);
+      return res.status(200).send(await response.text());
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
+
+  app.get('/mongodb/users', async (req, res) => {
+    try {
+      const database = mongodb_client.db('sample_mflix');
+      const collection = database.collection('users');
+
+      const users = await collection.find({}).toArray();
+
+      return res.status(200).json({ success: true, message: 'users', data: users });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`server listening at: http://${hostname}:${port}`);
+  });
+}
+
+try {
+  main();
+} catch (error) {
+  console.log('[Error]:', error);
+  await mongodb_client.close();
+}
