fe7207b5d0ed1bb1a9acb9eabadfaaf386fbf771
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-12-26 13:08:37 +0530

    feat: update

--- src/kubes.js
+++ src/kubes.js
@@ -1,100 +1,73 @@
-import 'dotenv/config';
-import express from 'express';
-import os from 'node:os';
-import { MongoClient } from 'mongodb';
+import "dotenv/config";
+import express from "express";
+import os from "node:os";
 
 let config = {
-  app_env: process.env.APP_ENV ?? 'LOCAL',
-  version: process.env.VERSION ?? 'LOCAL',
-  hostname: 'localhost',
-  port: process.env.PORT ?? 3002,
-  mongodb_url:
-    process.env.MONGODB_URL ??
-    'mongodb+srv://arijit:QffrRzYEHvCLt7Pv@demo-eks-cluster-1.ldann.mongodb.net/?retryWrites=true&w=majority&appName=demo-eks-cluster-1',
+	app_env: process.env.APP_ENV ?? "LOCAL",
+	version: process.env.VERSION ?? "LOCAL",
+	hostname: "localhost",
+	port: process.env.PORT ?? 3002,
 };
 
-if (process.env.APP_SECRET_PROVIDER === 'aws_secretsmanager') {
-  const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
-  config = {
-    app_env: aws_secetsmanager_secret.APP_ENV,
-    version: aws_secetsmanager_secret.VERSION,
-    hostname: 'localhost',
-    port: aws_secetsmanager_secret.PORT,
-    mongodb_url: aws_secetsmanager_secret.MONGODB_URL,
-  };
+if (process.env.APP_SECRET_PROVIDER === "aws_secretsmanager") {
+	const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
+	config = {
+		app_env: aws_secetsmanager_secret.APP_ENV,
+		version: aws_secetsmanager_secret.VERSION,
+		hostname: "localhost",
+		port: aws_secetsmanager_secret.PORT,
+	};
 }
 
-const app_env = config.app_env;
-const version = config.version;
-const hostname = config.hostname;
-const port = config.port;
-const mongodb_url = config.mongodb_url;
-
-const app = express();
-const mongodb_client = new MongoClient(mongodb_url);
-
-await mongodb_client.connect();
-
 async function main() {
-  app.get('/', async (req, res) => {
-    try {
-      return res.status(200).json({
-        success: true,
-        message: 'Hello kubes !',
-        app_env: app_env,
-        version: version,
-        os_hostname: os.hostname(),
-        arch: os.arch(),
-        APP_SECRET_PROVIDER: process.env.APP_SECRET_PROVIDER,
-      });
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error?.message });
-    }
-  });
+	const app = express();
 
-  app.get('/nginx', async (req, res) => {
-    try {
-      const url = 'http://nginx:80';
-      const response = await fetch(url);
-      return res.status(200).send(await response.text());
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error?.message });
-    }
-  });
+	// routes
+	app.get("/", async (req, res) => {
+		try {
+			return res.status(200).json({
+				success: true,
+				message: "Hello kubes !",
+				app_env: config.app_env,
+				version: config.version,
+				os_hostname: os.hostname(),
+				arch: os.arch(),
+				APP_SECRET_PROVIDER: process.env.APP_SECRET_PROVIDER,
+			});
+		} catch (error) {
+			return res.status(500).json({ success: false, message: error?.message });
+		}
+	});
 
-  app.get('/k8s', async (req, res) => {
-    try {
-      const url = 'http://k8s:3001';
-      const response = await fetch(url);
-      return res.status(200).send(await response.text());
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error?.message });
-    }
-  });
+	app.get("/nginx", async (req, res) => {
+		try {
+			const url = "http://nginx:80";
+			const response = await fetch(url);
+			return res.status(200).send(await response.text());
+		} catch (error) {
+			return res.status(500).json({ success: false, message: error?.message });
+		}
+	});
 
-  app.get('/mongodb/users', async (req, res) => {
-    try {
-      const database = mongodb_client.db('sample_mflix');
-      const collection = database.collection('users');
+	app.get("/k8s", async (req, res) => {
+		try {
+			const url = "http://k8s:3001";
+			const response = await fetch(url);
+			return res.status(200).send(await response.text());
+		} catch (error) {
+			return res.status(500).json({ success: false, message: error?.message });
+		}
+	});
 
-      const users = await collection.find({}).toArray();
-
-      return res
-        .status(200)
-        .json({ success: true, message: 'users', data: users });
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error?.message });
-    }
-  });
-
-  app.listen(port, () => {
-    console.log(`server listening at: http://${hostname}:${port}`);
-  });
+	app.listen(config.port, () => {
+		console.log(
+			`server listening at: http://${config.hostname}:${config.port}`,
+		);
+	});
 }
 
 try {
-  main();
+	main();
 } catch (error) {
-  console.log('[Error]:', error);
-  await mongodb_client.close();
+	console.log("[Error]:", error);
 }
