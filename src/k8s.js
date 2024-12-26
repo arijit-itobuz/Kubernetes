3dcef08dbfb844d8ae351e1e64275a8c6cfa7af7
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-12-26 15:53:39 +0530

    feat: k8s updated

--- src/k8s.js
+++ src/k8s.js
@@ -1,129 +1,131 @@
-import "dotenv/config";
-import express from "express";
-import os from "node:os";
-import { MongoClient } from "mongodb";
-import redis from "redis";
+import 'dotenv/config';
+import express from 'express';
+import os from 'node:os';
+import { MongoClient } from 'mongodb';
+import redis from 'redis';
 
 // base config
 let config = {
-	app_env: process.env.APP_ENV ?? "LOCAL",
-	version: process.env.VERSION ?? "LOCAL",
-	hostname: "localhost",
-	port: process.env.PORT ?? 3001,
-	mongodb_url:
-		process.env.MONGODB_URL ??
-		"mongodb+srv://arijit:QffrRzYEHvCLt7Pv@demo-eks-cluster-1.ldann.mongodb.net/?retryWrites=true&w=majority&appName=demo-eks-cluster-1",
+  app_env: process.env.APP_ENV ?? 'LOCAL',
+  version: process.env.APP_VERSION ?? 'LOCAL',
+  hostname: 'localhost',
+  port: process.env.APP_PORT ?? 3001,
+  mongodb_url:
+    process.env.MONGODB_URL ??
+    'mongodb+srv://arijit:QffrRzYEHvCLt7Pv@demo-eks-cluster-1.ldann.mongodb.net/?retryWrites=true&w=majority&appName=demo-eks-cluster-1',
+  redis_host: process.env.REDIS_HOST,
+  redis_port: process.env.REDIS_PORT,
+  redis_password: process.env.REDIS_PASSWORD,
 };
 
 // aws secretmanager config
-if (process.env.APP_SECRET_PROVIDER === "aws_secretsmanager") {
-	const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
-	config = {
-		app_env: aws_secetsmanager_secret.APP_ENV,
-		version: aws_secetsmanager_secret.VERSION,
-		hostname: "localhost",
-		port: aws_secetsmanager_secret.PORT,
-		mongodb_url: aws_secetsmanager_secret.MONGODB_URL,
-	};
+if (process.env.APP_SECRET_PROVIDER === 'aws_secretsmanager') {
+  const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
+  config = {
+    app_env: aws_secetsmanager_secret.APP_ENV,
+    version: aws_secetsmanager_secret.VERSION,
+    hostname: 'localhost',
+    port: aws_secetsmanager_secret.PORT,
+    mongodb_url: aws_secetsmanager_secret.MONGODB_URL,
+    redis_host: aws_secetsmanager_secret.REDIS_HOST,
+    redis_port: aws_secetsmanager_secret.REDIS_PORT,
+    redis_password: aws_secetsmanager_secret.REDIS_PASSWORD,
+  };
 }
 
 // clients
 const mongodb_client = new MongoClient(config.mongodb_url);
 const redis_client = redis.createClient({
-	socket: { host: "localhost", port: 6379 },
-	password: "redis-pass",
+  socket: { host: config.redis_host, port: config.redis_port },
+  password: config.redis_password,
 });
 
 // main
 async function main() {
-	const app = express();
-	await mongodb_client.connect();
-	await redis_client.connect();
+  const app = express();
+  await mongodb_client.connect();
+  await redis_client.connect();
 
-	// routes
-	app.get("/", async (req, res) => {
-		try {
-			return res.status(200).json({
-				success: true,
-				message: "Hello k8s !",
-				app_env: config.app_env,
-				version: config.version,
-				os_hostname: os.hostname(),
-				arch: os.arch(),
-				APP_SECRET_PROVIDER: process.env.APP_SECRET_PROVIDER,
-			});
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  // routes
+  app.get('/', async (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        message: 'Hello k8s !',
+        app: {
+          app_secret_provider: process.env.APP_SECRET_PROVIDER ?? 'N/A',
+          app_env: config.app_env,
+          app_version: config.version,
+          os_hostname: os.hostname(),
+          arch: os.arch(),
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/nginx", async (req, res) => {
-		try {
-			const url = "http://nginx:80";
-			const response = await fetch(url);
-			return res.status(200).send(await response.text());
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/nginx', async (req, res) => {
+    try {
+      const url = 'http://nginx:80';
+      const response = await fetch(url);
+      return res.status(200).send(await response.text());
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/kubes", async (req, res) => {
-		try {
-			const url = "http://kubes:3002";
-			const response = await fetch(url);
-			return res.status(200).send(await response.text());
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/kubes', async (req, res) => {
+    try {
+      const url = 'http://kubes:3002';
+      const response = await fetch(url);
+      return res.status(200).send(await response.text());
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/mongodb", async (req, res) => {
-		try {
-			const adminDb = mongodb_client.db().admin();
-			const databases = await adminDb.listDatabases();
-			const databaseNames = databases.databases.map((db) => db.name);
-			return res.status(200).json({ success: true, databases: databaseNames });
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/mongodb', async (req, res) => {
+    try {
+      const adminDb = mongodb_client.db().admin();
+      const databases = await adminDb.listDatabases();
+      const databaseNames = databases.databases.map((db) => db.name);
+      return res.status(200).json({ success: true, databases: databaseNames });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/redis", async (req, res) => {
-		try {
-			const response = await redis_client.ping();
-			return res
-				.status(200)
-				.json({ success: true, message: "Redis is alive!", response });
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/redis', async (req, res) => {
+    try {
+      const response = await redis_client.ping();
+      return res.status(200).json({ success: true, message: 'Redis is alive!', response });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/mongodb/users", async (req, res) => {
-		try {
-			const database = mongodb_client.db("sample_mflix");
-			const collection = database.collection("users");
+  app.get('/mongodb/users', async (req, res) => {
+    try {
+      const database = mongodb_client.db('sample_mflix');
+      const collection = database.collection('users');
 
-			const users = await collection.find({}).toArray();
+      const users = await collection.find({}).toArray();
 
-			return res
-				.status(200)
-				.json({ success: true, message: "users", data: users });
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+      return res.status(200).json({ success: true, message: 'users', data: users });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.listen(config.port, () => {
-		console.log(
-			`server listening at: http://${config.hostname}:${config.port}`,
-		);
-	});
+  app.listen(config.port, () => {
+    console.log(`server listening at: http://${config.hostname}:${config.port}`);
+  });
 }
 
 try {
-	main();
+  main();
 } catch (error) {
-	console.log("[Error]:", error);
-	await mongodb_client.close();
+  console.log('[Error]:', error);
+  await mongodb_client.close();
 }
--- src/kubes.js
+++ src/kubes.js
@@ -1,73 +1,73 @@
-import "dotenv/config";
-import express from "express";
-import os from "node:os";
+import 'dotenv/config';
+import express from 'express';
+import os from 'node:os';
 
 let config = {
-	app_env: process.env.APP_ENV ?? "LOCAL",
-	version: process.env.VERSION ?? "LOCAL",
-	hostname: "localhost",
-	port: process.env.PORT ?? 3002,
+  app_env: process.env.APP_ENV ?? 'LOCAL',
+  version: process.env.APP_VERSION ?? 'LOCAL',
+  hostname: 'localhost',
+  port: process.env.PORT ?? 3002,
 };
 
-if (process.env.APP_SECRET_PROVIDER === "aws_secretsmanager") {
-	const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
-	config = {
-		app_env: aws_secetsmanager_secret.APP_ENV,
-		version: aws_secetsmanager_secret.VERSION,
-		hostname: "localhost",
-		port: aws_secetsmanager_secret.PORT,
-	};
+if (process.env.APP_SECRET_PROVIDER === 'aws_secretsmanager') {
+  const aws_secetsmanager_secret = JSON.parse(process.env.APP_SECRETS);
+  config = {
+    app_env: aws_secetsmanager_secret.APP_ENV,
+    version: aws_secetsmanager_secret.VERSION,
+    hostname: 'localhost',
+    port: aws_secetsmanager_secret.PORT,
+  };
 }
 
 async function main() {
-	const app = express();
+  const app = express();
 
-	// routes
-	app.get("/", async (req, res) => {
-		try {
-			return res.status(200).json({
-				success: true,
-				message: "Hello kubes !",
-				app_env: config.app_env,
-				version: config.version,
-				os_hostname: os.hostname(),
-				arch: os.arch(),
-				APP_SECRET_PROVIDER: process.env.APP_SECRET_PROVIDER,
-			});
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  // routes
+  app.get('/', async (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        message: 'Hello kubes !',
+        app: {
+          app_secret_provider: process.env.APP_SECRET_PROVIDER ?? 'N/A',
+          app_env: config.app_env,
+          app_version: config.version,
+          os_hostname: os.hostname(),
+          arch: os.arch(),
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/nginx", async (req, res) => {
-		try {
-			const url = "http://nginx:80";
-			const response = await fetch(url);
-			return res.status(200).send(await response.text());
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/nginx', async (req, res) => {
+    try {
+      const url = 'http://nginx:80';
+      const response = await fetch(url);
+      return res.status(200).send(await response.text());
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.get("/k8s", async (req, res) => {
-		try {
-			const url = "http://k8s:3001";
-			const response = await fetch(url);
-			return res.status(200).send(await response.text());
-		} catch (error) {
-			return res.status(500).json({ success: false, message: error?.message });
-		}
-	});
+  app.get('/k8s', async (req, res) => {
+    try {
+      const url = 'http://k8s:3001';
+      const response = await fetch(url);
+      return res.status(200).send(await response.text());
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error?.message });
+    }
+  });
 
-	app.listen(config.port, () => {
-		console.log(
-			`server listening at: http://${config.hostname}:${config.port}`,
-		);
-	});
+  app.listen(config.port, () => {
+    console.log(`server listening at: http://${config.hostname}:${config.port}`);
+  });
 }
 
 try {
-	main();
+  main();
 } catch (error) {
-	console.log("[Error]:", error);
+  console.log('[Error]:', error);
 }
